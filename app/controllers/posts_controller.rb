f52b75e10aa7d8fa8df90524cb00d6da419df8de
Rémi Pachet <rpachet@info-viciousx.iut.bx1>
2018-07-04 17:58:46 +0200

    add profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def index
     if params[:user_id]
-      @posts = Post.find_by(user_id: params[:user_id])
+      @posts = Post.where(user_id: params[:user_id])
     else
       @posts = Post.all.order(:title)
 
